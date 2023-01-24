aa8aafeedcd26ede2f5490eb3ce542bb53be0aba
Rick Choi <jinhwanlazy@gmail.com>
2023-01-24 17:00:41 +0900

    add boj_6839

---
+++ boj_6839/readme.md
@@ -0,0 +1,2 @@
+# [BOJ6839 - boj_6839](https://www.acmicpc.net/problem/6839)
+<!--tags: geom, voronoi diagram-->
---
+++ boj_6839/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// see https://www.acmicpc.net/source/54612666
+
+void solve() {
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int T = 1;
+  while (T--) {
+    solve();
+  }
+
+  return 0;
+}
